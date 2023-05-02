154510448d91cea8061e0b9b47f1e738a818c5e9
Jesus Diaz <jdiazndca21@gmail.com>
2023-05-02 00:09:22 -0500

    Add project files.

---
+++ ScientificCalculator/Calculator.aspx
@@ -0,0 +1,86 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Calculator.aspx.cs" Inherits="ScientificCalculator.Calculator" %>
+  
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title>Calculator</title>
+    <style>
+        .set {
+        }
+        .inputBox {
+            margin-top: 20px;
+        }
+        .auto-style1 {
+            font-size: x-large;
+            margin-right: 5px;
+        }
+        .auto-style2 {}
+    </style>
+</head>
+<body>
+   <center>
+    <form id="form1" runat="server">
+        <div style="text-align: center">
+        <asp:Label ID="Label1" runat="server" Text="Calculator" Font-Bold="True" Font-Size="XX-Large"></asp:Label>
+        </div>
+        <p>
+            &nbsp;</p>
+        <div class="container m-4 p-4">
+            <center class="set" style="background-color:darkcyan; height: 538px; width: 480px;">
+                <br />
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                <asp:TextBox ID="resultsText" runat="server" CssClass="auto-style2" Height="16px" ReadOnly="True" Width="233px"></asp:TextBox>
+                <br />
+                <asp:TextBox class="inputBox" ID="inputBox" runat="server" Height="52px" style="text-align: center" BorderStyle="Groove" Width="391px" readonly="True"></asp:TextBox>
+                <br />
+                <br />
+                <asp:Button ID="oneBtn" runat="server" CssClass="auto-style1" Font-Bold="True" Height="54px" Text="1" Width="63px" OnClick="oneBtn_Click" />
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                <asp:Button ID="twoBtn" runat="server" CssClass="auto-style1" Font-Bold="True" Height="54px" OnClick="Button2_Click" Text="2" Width="63px" />
+                &nbsp; &nbsp;&nbsp;&nbsp;&nbsp;&nbsp; &nbsp;<asp:Button ID="threeBtn" runat="server" CssClass="auto-style1" Font-Bold="True" Height="54px" Text="3" Width="63px" OnClick="threeBtn_Click" />
+                &nbsp;&nbsp;&nbsp;&nbsp; &nbsp;&nbsp;&nbsp;&nbsp; &nbsp;
+                <asp:Button ID="backBtn" runat="server" CssClass="auto-style1" Font-Bold="True" Height="54px" Text="&lt;" Width="63px" OnClick="Button4_Click" />
+                <br />
+                <br />
+                <asp:Button ID="fourBtn" runat="server" CssClass="auto-style1" Font-Bold="True" Height="54px" Text="4" Width="65px" OnClick="fourBtn_Click" />
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                <asp:Button ID="fiveBtn" runat="server" CssClass="auto-style1" Font-Bold="True" Height="54px" Text="5" Width="63px" OnClick="fiveBtn_Click" />
+                &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                <asp:Button ID="sixBtn" runat="server" CssClass="auto-style1" Font-Bold="True" Height="54px" Text="6" Width="63px" OnClick="sixBtn_Click" />
+&nbsp;&nbsp; &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                <asp:Button ID="divBtn" runat="server" CssClass="auto-style1" Font-Bold="True" Height="54px" Text="÷" Width="63px" OnClick="divBtn_Click" />
+                <br />
+                <br />
+                <asp:Button ID="sevenBtn" runat="server" CssClass="auto-style1" Font-Bold="True" Height="54px" Text="7" Width="63px" OnClick="sevenBtn_Click" />
+            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                <asp:Button ID="eightBtn" runat="server" CssClass="auto-style1" Font-Bold="True" Height="54px" Text="8" Width="63px" OnClick="eightBtn_Click" />
+            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                <asp:Button ID="nineBtn" runat="server" CssClass="auto-style1" Font-Bold="True" Height="54px" Text="9" Width="63px" OnClick="nineBtn_Click" />
+            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; &nbsp;&nbsp;&nbsp;
+                <asp:Button ID="multiBtn" runat="server" CssClass="auto-style1" Font-Bold="True" Height="54px" Text="x" Width="63px" OnClick="multiBtn_Click" />
+                <br />
+                <br />
+                <asp:Button ID="posOrNegBtn" runat="server" CssClass="auto-style1" Font-Bold="True" Height="54px" Text="±" Width="63px" OnClick="posOrNegBtn_Click" />
+            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                <asp:Button ID="zeroBtn" runat="server" CssClass="auto-style1" Font-Bold="True" Height="54px" Text="0" Width="63px" OnClick="zeroBtn_Click" />
+            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; &nbsp;
+                <asp:Button ID="decimalBtn" runat="server" CssClass="auto-style1" Font-Bold="True" Height="54px" Text="." Width="63px" OnClick="decimalBtn_Click" />
+            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp; &nbsp;&nbsp;&nbsp;&nbsp;
+                <asp:Button ID="plusBtn" runat="server" CssClass="auto-style1" Font-Bold="True" Height="54px" Text="+" Width="63px" OnClick="plusBtn_Click" />
+                <br />
+                <br />
+                <asp:Button ID="xSqrBtn" runat="server" CssClass="auto-style1" Font-Bold="True" Height="54px" Text="x²" Width="63px" OnClick="xSqrBtn_Click" />
+            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                <asp:Button ID="clearBtn" runat="server" CssClass="auto-style1" Font-Bold="True" Height="54px" Text="C" Width="63px" OnClick="clearBtn_Click" />
+                &nbsp;&nbsp;&nbsp;&nbsp;&nbsp; &nbsp;&nbsp;
+                <asp:Button ID="equalBtn" runat="server" CssClass="auto-style1" Font-Bold="True" Height="54px" Text="=" Width="63px" OnClick="equalBtn_Click" />
+            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; &nbsp;&nbsp;
+                <asp:Button ID="minusBtn" runat="server" CssClass="auto-style1" Font-Bold="True" Height="54px" Text="-" Width="63px" OnClick="minusBtn_Click" />
+            </center>
+        </div>
+    </form>
+  </center>
+</body>
+</html>
---
+++ ScientificCalculator/Calculator.aspx.cs
@@ -0,0 +1,349 @@
+﻿using Antlr.Runtime.Tree;
+using Microsoft.Ajax.Utilities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Web;
+using System.Web.Optimization;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace ScientificCalculator
+{
+    public partial class Calculator : System.Web.UI.Page
+    {
+        static double a, c, d;
+        static char  b;
+        static string[] operatorList = {"*","/","+","-"};
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void oneBtn_Click(object sender, EventArgs e)
+        {
+            if ((inputBox.Text=="+") || (inputBox.Text=="-") || (inputBox.Text=="*") || (inputBox.Text=="/"))
+            {
+                inputBox.Text = "";
+                inputBox.Text = inputBox.Text + oneBtn.Text;
+            }
+            else {
+                inputBox.Text = inputBox.Text + oneBtn.Text;
+            }
+        }
+
+        protected void Button2_Click(object sender, EventArgs e)
+        {
+            if ((inputBox.Text=="+") || (inputBox.Text=="-") || (inputBox.Text=="*") || (inputBox.Text=="/"))
+            {
+                inputBox.Text = "";
+                inputBox.Text = inputBox.Text + twoBtn.Text;
+            }
+            else
+            {
+                inputBox.Text = inputBox.Text + twoBtn.Text;
+            }
+        }
+
+        protected void threeBtn_Click(object sender, EventArgs e)
+        {
+            if ((inputBox.Text=="+") || (inputBox.Text=="-") || (inputBox.Text=="*") || (inputBox.Text=="/"))
+            {
+                inputBox.Text = "";
+                inputBox.Text = inputBox.Text + threeBtn.Text;
+            }
+            else
+            {
+                inputBox.Text = inputBox.Text + threeBtn.Text;
+            }
+        }
+                        
+        //Backspace button
+        protected void Button4_Click(object sender, EventArgs e)
+        {
+            //check how many characters it has
+            int characterCount = 0;
+            foreach (char letter in inputBox.Text)
+            {
+                characterCount++;
+            }
+
+            //Checks if input text is empty
+            if (inputBox.Text == "")
+            {
+                inputBox.Text = "";
+            }
+            //If the current input contains an operator sign
+            //Then make no changes to its value
+            else if (operatorList.Contains(inputBox.Text)) {
+                inputBox.Text = inputBox.Text;
+            }
+            //Uses characterCount to check if it only has one character, so 
+            // it can be set to zero or empty and not get any errors.
+            else if (characterCount == 1)
+            {
+                inputBox.Text="";
+            }
+            //If characterCount is more than one then user can proceed to remove 
+            //the characters he had typed per each 
+            else if (characterCount > 1)
+            {
+
+                char[] inputBoxArray = inputBox.Text.ToCharArray();
+                char[] newInputBoxString = new char[inputBoxArray.Length-1];
+                string newString = "";
+
+                for (int index = 0; index < inputBoxArray.Length-1; index++)
+                {
+                    newInputBoxString[index] = inputBoxArray[index];
+                }
+                foreach (char letter in newInputBoxString)
+                {
+                    newString += letter;
+                }
+
+                inputBox.Text = newString;
+            }
+        }
+
+        protected void fourBtn_Click(object sender, EventArgs e)
+        {
+            if ((inputBox.Text=="+") || (inputBox.Text=="-") || (inputBox.Text=="*") || (inputBox.Text=="/"))
+            {
+                inputBox.Text = "";
+                inputBox.Text = inputBox.Text + fourBtn.Text;
+            }
+            else
+            {
+                inputBox.Text = inputBox.Text + fourBtn.Text;
+            }
+        }
+
+        protected void fiveBtn_Click(object sender, EventArgs e)
+        {
+            if ((inputBox.Text=="+") || (inputBox.Text=="-") || (inputBox.Text=="*") || (inputBox.Text=="/"))
+            {
+                inputBox.Text = "";
+                inputBox.Text = inputBox.Text + fiveBtn.Text;
+            }
+            else
+            {
+                inputBox.Text = inputBox.Text + fiveBtn.Text;
+            }
+        }
+
+        protected void sixBtn_Click(object sender, EventArgs e)
+        {
+            if ((inputBox.Text=="+") || (inputBox.Text=="-") || (inputBox.Text=="*") || (inputBox.Text=="/"))
+            {
+                inputBox.Text = "";
+                inputBox.Text = inputBox.Text + sixBtn.Text;
+            }
+            else
+            {
+                inputBox.Text = inputBox.Text + sixBtn.Text;
+            }
+        }
+        //Division button
+        protected void divBtn_Click(object sender, EventArgs e)
+        {
+                a = Convert.ToDouble(inputBox.Text);
+                inputBox.Text = "";
+                b = '/';
+                inputBox.Text += b;
+        }
+
+        protected void sevenBtn_Click(object sender, EventArgs e)
+        {
+            if ((inputBox.Text=="+") || (inputBox.Text=="-") || (inputBox.Text=="*") || (inputBox.Text=="/"))
+            {
+                inputBox.Text = "";
+                inputBox.Text = inputBox.Text + sevenBtn.Text;
+            }
+            else
+            {
+                inputBox.Text = inputBox.Text + sevenBtn.Text;
+            }
+        }
+
+        protected void eightBtn_Click(object sender, EventArgs e)
+        {
+            if ((inputBox.Text=="+") || (inputBox.Text=="-") || (inputBox.Text=="*") || (inputBox.Text=="/"))
+            {
+                inputBox.Text = "";
+                inputBox.Text = inputBox.Text + eightBtn.Text;
+            }
+            else
+            {
+                inputBox.Text = inputBox.Text + eightBtn.Text;
+            }
+        }
+
+        protected void nineBtn_Click(object sender, EventArgs e)
+        {
+            if ((inputBox.Text=="+") || (inputBox.Text=="-") || (inputBox.Text=="*") || (inputBox.Text=="/"))
+            {
+                inputBox.Text = "";
+                inputBox.Text = inputBox.Text + nineBtn.Text;
+            }
+            else
+            {
+                inputBox.Text = inputBox.Text + nineBtn.Text;
+            }
+        }
+
+        //Multiplication button
+        protected void multiBtn_Click(object sender, EventArgs e)
+        {
+            a = Convert.ToDouble(inputBox.Text);
+            inputBox.Text = "";
+            b = '*';
+            inputBox.Text += b;
+        }
+
+        //Positive or Negative Sign
+        protected void posOrNegBtn_Click(object sender, EventArgs e)
+        {
+            //This is used to check if the current text is either empty
+            // Or if it contains an operator, so we can make no changes
+            if (inputBox.Text == "" || (operatorList.Contains(inputBox.Text)))
+            {
+                inputBox.Text=inputBox.Text;
+            }
+            //This is to check if the current character is not a operator
+            else if(!operatorList.Contains(inputBox.Text)){
+                //This button is to turn the current number to positive or negative
+                double number = double.Parse(inputBox.Text);
+                if (number < 0)
+                {
+                    //turn number to positive
+                    number = number * -1;
+                }
+                else if (number > 0)
+                {
+                    //turn number to negative
+                    number = number * -1;
+                }
+                inputBox.Text = number.ToString();
+            }
+        }
+
+        protected void zeroBtn_Click(object sender, EventArgs e)
+        {
+            if ((inputBox.Text=="+") || (inputBox.Text=="-") || (inputBox.Text=="*") || (inputBox.Text=="/"))
+            {
+                inputBox.Text = "";
+                inputBox.Text = inputBox.Text + zeroBtn.Text;
+            }
+            else
+            {
+                inputBox.Text = inputBox.Text + zeroBtn.Text;
+            }
+        }
+
+        protected void decimalBtn_Click(object sender, EventArgs e)
+        {
+            //Check for this one
+            if ((inputBox.Text=="+") || (inputBox.Text=="-") || (inputBox.Text=="*") || (inputBox.Text=="/"))
+            {
+                inputBox.Text = "";
+                inputBox.Text = inputBox.Text + decimalBtn.Text;
+            }
+            else
+            {
+                inputBox.Text = inputBox.Text + decimalBtn.Text;
+            }
+        }
+        
+        //Addition button
+        protected void plusBtn_Click(object sender, EventArgs e)
+        {
+            a = Convert.ToDouble(inputBox.Text);
+            inputBox.Text = "";
+            b = '+';
+            inputBox.Text += b;
+        }
+
+        //Squared Operation
+        protected void xSqrBtn_Click(object sender, EventArgs e)
+        {
+            //Square the current text value
+            double currentNum;
+            currentNum = Convert.ToDouble(inputBox.Text);
+            currentNum = currentNum * currentNum;
+            inputBox.Text = currentNum.ToString();
+
+        }
+
+        //Erase Button
+        protected void clearBtn_Click(object sender, EventArgs e)
+        {
+            //Clear all variable values and text box input
+            inputBox.Text="";
+            a=0;
+            c=0;
+            d=0;
+            b='\0';
+            resultsText.Text = "";
+        }
+
+        //Equals Button
+        protected void equalBtn_Click(object sender, EventArgs e)
+        {
+            resultsText.Text="";
+            //If nothing has been type then make no change
+            if (inputBox.Text == "") {
+                inputBox.Text = inputBox.Text;
+            }
+            //This is used to prevent the bug where it keeps adding itself again
+            //each time you click the equal sign
+            if (inputBox.Text == d.ToString()) {
+                inputBox.Text = inputBox.Text;
+            }
+            else {
+                c = Convert.ToDouble(inputBox.Text);
+                inputBox.Text="";
+                if (b == '/')
+                {
+                    d = a / c;
+                    inputBox.Text +=d;
+                    resultsText.Text = $"{a} / {c} = {d}";
+                    a = d;
+                }
+                else if (b == '*')
+                {
+                    d = a * c;
+                    inputBox.Text +=d;
+                    resultsText.Text = $"{a} * {c} = {d}";
+                    a = d;
+                }
+                else if (b == '+')
+                {
+                    d = a + c;
+                    inputBox.Text +=d;
+                    resultsText.Text = $"{a} + {c} = {d}";
+                    a = d;
+                }
+                else if (b == '-')
+                {
+                    d = a - c;
+                    inputBox.Text +=d;
+                    resultsText.Text = $"{a} - {c} = {d}";
+                    a = d;
+                }
+            }
+        }
+
+        //Substraction button
+        protected void minusBtn_Click(object sender, EventArgs e)
+        {
+            a = Convert.ToDouble(inputBox.Text);
+            inputBox.Text = "";
+            b = '-';
+            inputBox.Text += b;
+        }
+
+    }
+}
